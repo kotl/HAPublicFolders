1afd14cc3c8f24fc689340bae5175feb7a3c6f28
Sergei Kotlyachkov <kotl@users.noreply.github.com>
2024-08-13 21:02:46 -0400

    Update run.sh

--- public_folders/run.sh
+++ public_folders/run.sh
@@ -2,7 +2,7 @@
 
 set +u
 
-export PORT=$(bashio::addon.port 8080)
+export PORT=$(bashio::addon.port 8888)
 export FOLDERS=$(bashio::config "folders")
 
 bashio::log.info "Starting http service on port $PORT."
